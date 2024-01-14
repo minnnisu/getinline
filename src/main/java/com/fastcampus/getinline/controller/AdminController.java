df1eed1d30a40c336ee7c018d9cd84c5242049ab
minnnisu <mine52100@naver.com>
2024-01-14 16:23:32 +0900

    Add domain controllers and views

--- src/main/java/com/fastcampus/getinline/controller/AdminController.java
+++ src/main/java/com/fastcampus/getinline/controller/AdminController.java
@@ -1,16 +1,31 @@
 package com.fastcampus.getinline.controller;
 
+import com.fastcampus.getinline.constant.PlaceType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RequestMapping("/admin")
 @Controller
 public class AdminController {
     @GetMapping("/places")
-    public String adminPlace() {
-        return "admin/places";
+    public ModelAndView adminPlace(PlaceType placeType,
+                                   String placeName,
+                                   String address
+    ) {
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("placeType", placeType);
+        map.put("placeName", placeName);
+        map.put("address", address);
+
+
+        return new ModelAndView("admin/places", map);
     }
 
     @GetMapping("/places/{placeId}")
