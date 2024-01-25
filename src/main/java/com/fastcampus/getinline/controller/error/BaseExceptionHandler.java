960b9514319b2fc8f46c1d0dc0c938a3acd3ea87
minnnisu <mine52100@naver.com>
2024-01-25 23:27:22 +0900

    Add 404 error and pass a test

--- src/main/java/com/fastcampus/getinline/controller/error/BaseExceptionHandler.java
+++ src/main/java/com/fastcampus/getinline/controller/error/BaseExceptionHandler.java
@@ -2,13 +2,19 @@ package com.fastcampus.getinline.controller.error;
 
 import com.fastcampus.getinline.constant.ErrorCode;
 import com.fastcampus.getinline.exception.GeneralException;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.Map;
 
+@Slf4j
 @ControllerAdvice
 public class BaseExceptionHandler {
 
@@ -29,6 +35,23 @@ public class BaseExceptionHandler {
                 status
         );
     }
+
+    @ExceptionHandler(value = {NoResourceFoundException.class, NoHandlerFoundException.class})
+    public ModelAndView handle404Error(Exception e) {
+        ErrorCode errorCode = ErrorCode.NOT_FOUND;
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        return new ModelAndView(
+                "error",
+                Map.of(
+                        "statusCode", status.value(),
+                        "errorCode", errorCode,
+                        "message", errorCode.getMessage()
+                ),
+                status
+        );
+    }
+
 
     @ExceptionHandler
     public ModelAndView exception(Exception e) {
